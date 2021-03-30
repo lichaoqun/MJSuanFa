853fe3542d547e26eb7777719eef7290292e3bf3
李超群 <121708983@qq.com>
2021-03-31 00:23:44 +0800

    xx

--- 04-栈/src/Main.java
+++ 04-栈/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import com.sun.net.httpserver.Authenticator.Result;
 
@@ -117,6 +118,46 @@ class Solution {
     	    	
     	return sb.toString();
     }
+    /* 496. 下一个更大元素 I
+     * https://leetcode-cn.com/problems/next-greater-element-i/
+     * */
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+    	java.util.Stack<Integer>stack = new java.util.Stack<>();
+    	// - 入栈
+    	for (int i = 0; i < nums1.length; i++) {
+			stack.push(nums1[i]);
+		}
+    	
+    	java.util.Stack<Integer>valStack = new java.util.Stack<>();
+    	while (!stack.isEmpty()) {
+    		int val = 0;
+			int stackVal = stack.peek();
+
+			for (int i = 0; i < nums2.length; i++) {
+				int curVal = nums2[i];
+				if (curVal == stackVal) {
+					stack.pop();
+					val = -1;
+				}else if (val == -1 && curVal > stackVal) {
+					
+					val = curVal;
+					i = nums2.length - 1;
+				}
+				
+				if ( i == nums2.length - 1) {
+					valStack.push(val);
+				}
+			}
+		}
+    	
+    	
+    	int[] valus = new int[valStack.size()];
+    	for (int i = 0; i < valus.length; i++) {
+    		valus[i] = valStack.pop();
+		}
+    	
+    	return valus;
+    }
     
 }
 public class Main {
@@ -135,6 +176,12 @@ public class Main {
 		System.out.println("---------------");
 		
 		System.out.println(solution.removeDuplicates("abbaca"));
+		
+		System.out.println("---------------");
+		
+		int[] num1 = {4,1,2};
+		int[] num2 = {1,3,4,2};
+		solution.nextGreaterElement(num1, num2);
 		
 		System.out.println("---------------");
 		
