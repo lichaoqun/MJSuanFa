6d9421536045856e7fa628cf4de8afc58d52392b
李超群 <121708983@qq.com>
2021-04-02 09:27:00 +0800

    xx

--- 00-leetcode/src/队列/_933_最近的请求次数.java
+++ 00-leetcode/src/队列/_933_最近的请求次数.java
@@ -13,7 +13,7 @@ import java.util.Queue;
 public class _933_最近的请求次数 {
 	private Queue <Integer> q = new LinkedList();
     public int ping(int t) {
-        q.add(t);
+        q.offer(t);
         while (q.peek() < t - 3000)
             q.poll();
         return q.size();
