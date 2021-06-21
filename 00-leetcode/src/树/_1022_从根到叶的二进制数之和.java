3a988a51a0971703654db17f1ea61a6178ebb3a6
李超群 <121708983@qq.com>
2021-06-21 10:42:59 +0800

    准备使用ideal打开

---
+++ 00-leetcode/src/树/_1022_从根到叶的二进制数之和.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package 树;
+
+/**
+ * @author lichaoqun
+ * https://leetcode-cn.com/problems/sum-of-root-to-leaf-binary-numbers/
+ */
+public class _1022_从根到叶的二进制数之和 {
+	public int sumRootToLeaf(TreeNode root) {
+        return 0;
+    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ 00-leetcode/src/树/_543_二叉树的直径.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package 树;
+
+/**
+ * @author lichaoqun
+ *
+ */
+public class _543_二叉树的直径 {
+
+    public int diameterOfBinaryTree(TreeNode root) {
+    	return 0;
+    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
